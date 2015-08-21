ff68bd3e3ef8f54cc1cc18eb9fac96c283d0e0a5
Chun-Ming Su <sokunmin@gmail.com>
2015-08-21 13:22:43 +0800

    Add copyright comment to L.java.

--- src/L.java
+++ src/L.java
@@ -1,3 +1,9 @@
+/*  L.java
+
+   Created by Chun-Ming Su on 8/20/15.
+   Copyright (c) 2015 Chun-Ming Su. All rights reserved.
+*/
+
 package im.chunmingsu.logsample;
 
 import android.content.Context;
